cdd9e6a5f618bb41d14c3e1fa21c1d1f5aff7c52
XFabio22 <98523846+XFabio22@users.noreply.github.com>
2022-07-13 02:00:20 -0700

    Sprint 5

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-// const HTMLResponse:any = document.querySelector("#joke")
 
 // async function cargarChiste() {
 //     const API_URL = "https://icanhazdadjoke.com/";
@@ -31,20 +30,82 @@
 //     console.log(chisteResuesta);
 
 // }
-function cargarChistes() {
+// ERRORE INTENTE HACER UN MAP Y DABA ERROR PORQUE NO DEVOLVIA UNA ARRAY 
+// NOTAS que arranque con un chiste y sacar el interface de la funcion cargar chiste 
+
+async function cargarChistes() {
+
+
     const API_URL = "https://icanhazdadjoke.com/";
     let confiApi = {
         headers: {
             Accept: "application/json",
         },
     };
-    fetch(API_URL, confiApi)
+   await fetch(API_URL, confiApi) // hace un peticion y devuelve el valor se asemeja a un forEach
     .then( response => response.json())
     .then(data => {
-      let element:any = document.getElementById('joke')
-      element.innerHTML = `<p>${data.joke} <p>`
-      console.log(data)
+      let element:any = document.getElementById('joke');
+      element.innerHTML = `<p>${data.joke}<p>`;
+        
+
+      // let puntos:any = document.getElementById('puntuacion');
+      // document.getElementById('puntuacion').innerHTML='';
+
+      // const puntos1 = document.createElement('p');
+      // puntos1.innerHTML = '<button class="btn btn-outline-danger m-1" onclick="puntuacion1('+data.id+')">😐</button>';
+      // puntos.appendChild(puntos1);
+
+      // const puntos2 = document.createElement('p');
+      // puntos2.innerHTML = '<button class="btn btn-outline-warning m-1" onclick="puntuacion2('+data.joke+')">🙂</button>';
+      // puntos.appendChild(puntos2);
+
+      // const puntos3 = document.createElement('p');
+      // puntos3.innerHTML = '<button class="btn btn-outline-success m-1" onclick="puntuacion3('+data.joke+')">😆</button>';
+      // puntos.appendChild(puntos3);
+      
     })
   
    
   }
+  
+  let reportJoke:any[] = []
+  let fechaActual = new Date;
+
+  cargarChistes()
+  interface ObjJoke{
+    jokeP: string;
+    score: number;
+    date : string;
+}
+  
+
+  class VOTEDJOKE implements ObjJoke {
+    jokeP: string;
+    score: number;
+    date : string;
+    constructor( jokeP: string,score: number, date : string){
+      this.jokeP = jokeP;
+      this.score = score;
+      this.date = date;
+    }
+  }
+ 
+
+  function puntuacion1(data){
+    let jokeP= data;
+    let score = 1;
+    let date = fechaActual.toISOString();
+    let joke1 = new VOTEDJOKE(jokeP,score,date);
+    reportJoke.push(joke1)
+    console.log(reportJoke);
+
+  }
+  function puntuacion2(){
+    
+  }
+  function puntuacion3(){
+    
+  }
+
+
